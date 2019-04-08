a8da73437acf2615ad1e71a467b5a4cfb6593fd8
NCC74656 <wolginmark@gmail.com>
2019-04-08 19:28:21 -0400

    Test Headers pt1

--- src/GameFiles/OneTestToRuleThemAll.java
+++ src/GameFiles/OneTestToRuleThemAll.java
@@ -4,11 +4,69 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class OneTestToRuleThemAll {
-
+	
 	@Test
-	void test() {
-		//fail("Not yet implemented");
+	void testAirCurrent() {
+		
+	}
+	
+	@Test
+	void testBird() {
 		assertEquals(true, true);
 	}
 
+	@Test
+	void testBush() {
+			
+	}
+	
+	@Test
+	void testClapperRail() {
+		
+	}
+	
+	@Test
+	void testCollectable() {
+		
+	}
+	
+	@Test
+	void testControllable() {
+		
+	}
+	
+	@Test
+	void testController() {
+		
+	}
+	
+	@Test
+	void testFish() {
+		
+	}
+	
+	@Test
+	void testFox() {
+		
+	}
+	
+	@Test
+	void testGameObject() {
+		
+	}
+	
+	@Test
+	void testInvisibleWall() {
+		
+	}
+	
+	@Test
+	void testModel() {
+		
+	}
+	
+	@Test
+	void testNonControllable() {
+		
+	}
 }
