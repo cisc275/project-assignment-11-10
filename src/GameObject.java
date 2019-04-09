62c65583498d68788631f06d75e53ed285ebb0fd
jennyb1 <46670398+jennyb1@users.noreply.github.com>
2019-04-08 20:00:20 -0400

    gameobject get/set

---
+++ src/GameObject.java
@@ -0,0 +1,213 @@
+package GameFiles;
+import java.io.File;
+/**
+ * Super class for every object in the game  
+ *
+ */
+public class GameObject {
+	/**
+	 * y coordinate for this gameObject on screen
+	 */
+	private int y;
+	/**
+	 * x coordinate for this gameObject on screen
+	 */
+	private int x; 
+	/**
+	 * image that will represent the gameObject on screen
+	 */
+	private File imgPose; 
+	/**
+	 * width of the gameObject on screen
+	 */
+	private int width; 
+	/**
+	 * height of the gameObject on screen
+	 */
+	private int height;
+	/**
+	 * minimum x coordinate for this gameObject
+	 */
+	private int xMin; 
+	/**
+	 * maximum x coordinate for this gameObject
+	 */
+	private int xMax; 
+	/**
+	 * minimum y coordinate for this gameObject
+	 */
+	private int yMin;
+	/**
+	 * maximum y coordinate for this gameObject
+	 */
+	private int yMax; 
+
+	/**
+	 * @param y
+	 * @param x
+	 * @param imgPose
+	 * @param width
+	 * @param height
+	 * @param xMin
+	 * @param xMax
+	 * @param yMin
+	 * @param yMaxa
+	 * 
+	 * a constructor that takes values for all fields as input parameters
+	 */
+	public GameObject(int y, int x, File imgPose, int width, int height, int xMin, 
+			int xMax, int yMin, int yMax) {
+	}
+
+	/**
+	 * 
+	 * @param a
+	 * @return true if there's a collision false otherwise
+	 * 
+	 * this method compares the x and y coordinates of the gameObjects also 
+	 * taking into consideration each ones width and heights to check whether 
+	 * or not there has been a collision
+	 * 
+	 */
+	public boolean collidesWith(GameObject a) {
+
+		return true;
+	}
+	/**
+	 * 
+	 * parameters: None
+	 * returns: None
+	 *  
+	 *  this method will be defined in subclasses so that each object knows
+	 *  how to handle a collision in the even collidesWith returns true.
+	 */
+	public void collisions() {
+
+	}
+
+	/**
+	 * @return the y
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	/**
+	 * @return the x
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * @param x the x to set
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	/**
+	 * @return the imgPose
+	 */
+	public File getImgPose() {
+		return imgPose;
+	}
+
+	/**
+	 * @param imgPose the imgPose to set
+	 */
+	public void setImgPose(File imgPose) {
+		this.imgPose = imgPose;
+	}
+
+	/**
+	 * @return the width
+	 */
+	public int getWidth() {
+		return width;
+	}
+
+	/**
+	 * @param width the width to set
+	 */
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	/**
+	 * @return the height
+	 */
+	public int getHeight() {
+		return height;
+	}
+
+	/**
+	 * @param height the height to set
+	 */
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	/**
+	 * @return the xMin
+	 */
+	public int getxMin() {
+		return xMin;
+	}
+
+	/**
+	 * @param xMin the xMin to set
+	 */
+	public void setxMin(int xMin) {
+		this.xMin = xMin;
+	}
+
+	/**
+	 * @return the xMax
+	 */
+	public int getxMax() {
+		return xMax;
+	}
+
+	/**
+	 * @param xMax the xMax to set
+	 */
+	public void setxMax(int xMax) {
+		this.xMax = xMax;
+	}
+
+	/**
+	 * @return the yMin
+	 */
+	public int getyMin() {
+		return yMin;
+	}
+
+	/**
+	 * @param yMin the yMin to set
+	 */
+	public void setyMin(int yMin) {
+		this.yMin = yMin;
+	}
+
+	/**
+	 * @return the yMax
+	 */
+	public int getyMax() {
+		return yMax;
+	}
+
+	/**
+	 * @param yMax the yMax to set
+	 */
+	public void setyMax(int yMax) {
+		this.yMax = yMax;
+	}
+}
