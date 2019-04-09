d0b09539dfe9de4e390331570a9405df5490469e
Andrew Thompson <47996175+afthompson@users.noreply.github.com>
2019-04-08 20:28:25 -0400

    added tests

--- src/GameFiles/OneTestToRuleThemAll.java
+++ src/GameFiles/OneTestToRuleThemAll.java
@@ -6,127 +6,50 @@ import org.junit.jupiter.api.Test;
 class OneTestToRuleThemAll {
 	
 	@Test
-	void testAirCurrent() {
-		
-	}
-	
-	@Test
-	void testBird() {
-		assertEquals(true, true);
-	}
-
-	@Test
-	void testBush() {
-			
-	}
-	
-	@Test
-	void testClapperRail() {
-		
-	}
-	
-	@Test
-	void testCollectable() {
-		
-	}
-	
-	@Test
-	void testControllable() {
-		
-	}
-	
-	@Test
-	void testController() {
-		
-	}
-	
-	@Test
-	void testFish() {
-		
-	}
-	
-	@Test
-	void testFox() {
-		
-	}
-	
-	@Test
-	void testGameObject() {
-		
-	}
-	
-	@Test
-	void testInvisibleWall() {
-		
-	}
-	
-	@Test
-	void testModel() {
-		
-	}
-	
-	@Test
-	void testNonControllable() {
-		
-	}
-	
-	@Test
-	void testOsprey() {
-		
-	}
-	
-	@Test
-	void testQuiz() {
-		
-	}
-	
-	@Test
 	void testSelectionModel() {
 		
 	}
 	
 	@Test
-	void testSelectionView() {
-		
-	}
-	
-	@Test
 	void testSideScrollModel() {
+		SideScrollModel scm = new SideScrollModel();
+		ArrayList<GameObject> go = new ArrayList<GameObject>();
+		go.add(new Osprey());
+		gp.add(new Fish());
+		int oldx1 = scm.getGame().get(0).getX();
+		int oldy1 = scm.getGame().get(0).getY();
+		int oldx2 = scm.getGame().get(1).getX();
+		int oldy2 = scm.getGame().get(1).getY();
+		scm.setGame(go);
+		scm.advanceWorld();
+		assertNotEquals(oldx1,scm.getGame().get(0).getX());
+		assertNotEquals(oldy1,scm.getGame().get(0).getY());	
+		assertNotEquals(oldx2,scm.getGame().get(1).getX());
+		assertEquals(oldy2,scm.getGame().get(1).getY());	
+		
 		
 	}
-	
-	@Test
-	void testSideScrollView() {
-		
-	}
-	
-	@Test
-	void testStick() {
-		
-	}
-	
+
 	@Test
 	void testTopDownModel() {
-		
+		TopDownModel tdm = new TopDownModel();
+		ArrayList<GameObject> go = new ArrayList<GameObject>();
+		go.add(new ClapperRail());
+		go.add(new Bush());
+		int oldx1 = tdm.getGame().get(0).getX();
+		int oldy1 = tdm.getGame().get(0).getY();
+		int oldx2 = tdm.getGame().get(1).getX();
+		int oldy2 = tdm.getGame().get(1).getY();
+		tdm.setGame(go);
+		tdm.update();
+		assertNotEquals(oldx1,tdm.getGame().get(0).getX());
+		assertNotEquals(oldy1,tdm.getGame().get(0).getY());
+		assertEquals(oldx2,tdm.getGame().get(1).getX());
+		assertEquals(oldy2,tdm.getGame().get(1).getY());
 	}
 	
 	@Test
 	void testTopDownView() {
-		
-	}
-	
-	@Test
-	void testTrash() {
-		
-	}
-	
-	@Test
-	void testType() {
-		
-	}
-	
-	@Test
-	void testView() {
 		
 	}
 }
