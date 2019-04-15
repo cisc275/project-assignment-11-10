7373ca8380463e7be2278e88239438f79fc8b3cf
melvinte <melvinte@udel.edu>
2019-04-15 18:35:32 -0400

    testing testing testing testing testing testing testing testing testing

--- src/GameFiles/OneTestToRuleThemAll.java
+++ src/GameFiles/OneTestToRuleThemAll.java
@@ -39,13 +39,16 @@ class OneTestToRuleThemAll {
 		ArrayList<GameObject> go = new ArrayList<GameObject>();
 		go.add(new ClapperRail(0, 0, null, 0, 0, 0, 0, 0, 0, 0, null, false, false, false, 0, 0, 0));
 		go.add(new Bush(0, 0, null, 0, 0, 0, 0, 0, 0, false, 0, 0, 0));
+		go.add(new Bush(1000, 1000, null, 0, 0, 0, 0, 0, 0, false, 0, 0, 0));
 		tdm.setGame(go);
 		int oldx1 = tdm.getGame().get(0).getX();
 		int oldy1 = tdm.getGame().get(0).getY();
 		int oldx2 = tdm.getGame().get(1).getX();
 		int oldy2 = tdm.getGame().get(1).getY();
 		tdm.setGame(go);
+		tdm.collision(go);
 		tdm.update();
+		
 		assertNotEquals(oldx1,tdm.getGame().get(0).getX());
 		assertNotEquals(oldy1,tdm.getGame().get(0).getY());
 		assertEquals(oldx2,tdm.getGame().get(1).getX());
