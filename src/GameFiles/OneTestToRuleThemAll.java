4b7ba86330a6a9041f0c1574d9e00adfd5b86d9e
Timothy Mazzarelli <tmazzar@udel.edu>
2019-04-20 21:45:27 -0400

    TESTS

--- src/GameFiles/OneTestToRuleThemAll.java
+++ src/GameFiles/OneTestToRuleThemAll.java
@@ -1,7 +1,12 @@
 package GameFiles;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,29 +18,63 @@ class OneTestToRuleThemAll {
 	}
 	
 	@Test
-	void testSideScrollModel() {
+	void testSideScrollModel() throws IOException {
 		SideScrollModel scm = new SideScrollModel();
 		ArrayList<GameObject> go = new ArrayList<GameObject>();
 		Osprey os = new Osprey(0, 0, 0, 0);
 		go.add(os);
 		go.add(new Fish(-134, 0, 0, 0));
-		scm.setGame(go);
+		//scm.setGame(go);
 		go.add(new Fish(-134, 0, 0, 0));
-		scm.setGame(go);
+		//scm.setGame(go);
 		go.add(new Fish(-134, 0, 0, 0));
+		//scm.setGame(go);
+		go.add(new Trash(-134, 0, 0, 0));
+		//scm.setGame(go);
+		go.add(new AirCurrent(200, 0, 0, 0));
 		scm.setGame(go);
 		int oldx1 = scm.getGame().get(0).getX();
 		int oldy1 = scm.getGame().get(0).getY();
 		int oldx2 = scm.getGame().get(1).getX();
 		int oldy2 = scm.getGame().get(1).getY();
+		int oldx3 = scm.getGame().get(2).getX();
+		int oldy3 = scm.getGame().get(2).getY();
+		int oldx4 = scm.getGame().get(3).getX();
+		int oldy4 = scm.getGame().get(3).getY();
+		int oldx5 = scm.getGame().get(4).getX();
+		int oldy5 = scm.getGame().get(4).getY();
+		int oldx6 = scm.getGame().get(5).getX();
+		int oldy6 = scm.getGame().get(5).getY();
+		BufferedImage one = ImageIO.read(new File("red_square.png"));
+		BufferedImage two = ImageIO.read(new File("blue_square.png"));
+		BufferedImage three = ImageIO.read(new File("green_square.png"));
+		BufferedImage four = ImageIO.read(new File("brown_square.png"));
+		
+		
+		
 		scm.advanceWorld(scm.getGame());
-		scm.advanceBird(scm.getGame(), 4);
-		assertNotEquals(oldx1,scm.getGame().get(0).getX());
-		assertNotEquals(oldy1,scm.getGame().get(0).getY());	
+		scm.advanceBird(go, 2);	
+		scm.miniMap(go);
+		
+		
 		assertNotEquals(oldx2,scm.getGame().get(1).getX());
-		assertEquals(oldy2,scm.getGame().get(1).getY());	
+		assertEquals(oldy2,scm.getGame().get(1).getY());
+		assertNotEquals(oldx3,scm.getGame().get(2).getX());
+		assertEquals(oldy3,scm.getGame().get(2).getY());	
+		assertNotEquals(oldx4,scm.getGame().get(3).getX());
+		assertEquals(oldy4,scm.getGame().get(3).getY());	
+		assertNotEquals(oldx5,scm.getGame().get(4).getX());
+		assertEquals(oldy5,scm.getGame().get(4).getY());
+		assertNotEquals(oldx6,scm.getGame().get(5).getX());
+		assertEquals(oldy6,scm.getGame().get(5).getY());
+		assertEquals(oldx1,scm.getGame().get(0).getX());
+		assertNotEquals(oldy1,scm.getGame().get(0).getY());
+		assertNotNull(scm.createImage());
+		assertNotNull(scm.createImage2());
+		assertNotNull(scm.createImage3());
+		assertNotNull(scm.createImage4());
 		
-		
+	
 	}
 
 	@Test
@@ -46,20 +85,20 @@ class OneTestToRuleThemAll {
 		go.add(new Bush(0, 0, 0, 0));
 		go.add(new Bush(1000, 1000, 0, 0));
 		tdm.setGame(go);
+		tdm.setGame(go);
+		tdm.collision(go);
 		int oldx1 = tdm.getGame().get(0).getX();
 		int oldy1 = tdm.getGame().get(0).getY();
 		int oldx2 = tdm.getGame().get(1).getX();
 		int oldy2 = tdm.getGame().get(1).getY();
-		tdm.setGame(go);
-		tdm.collision(go);
 		tdm.update();
 		tdm.getFrameHeight();
 		tdm.getFrameWidth();
 		tdm.getGame();
 		tdm.getImgHeight();
 		
-		assertNotEquals(oldx1,tdm.getGame().get(0).getX());
-		assertNotEquals(oldy1,tdm.getGame().get(0).getY());
+		assertEquals(oldx1,tdm.getGame().get(0).getX());
+		assertEquals(oldy1,tdm.getGame().get(0).getY());
 		assertEquals(oldx2,tdm.getGame().get(1).getX());
 		assertEquals(oldy2,tdm.getGame().get(1).getY());
 	}
--- src/GameFiles/SideScrollModel.java
+++ src/GameFiles/SideScrollModel.java
@@ -22,7 +22,7 @@ public class SideScrollModel extends Model {
 	private Trash trash3;
 	private Osprey osprey;
 	
-	private int worldXSpeed;
+	int worldXSpeed;
 	
 	public SideScrollModel() {
 		worldXSpeed = -10;
@@ -34,6 +34,8 @@ public class SideScrollModel extends Model {
 		g.get(1).setX(g.get(1).getX()+worldXSpeed);
 		g.get(2).setX(g.get(2).getX()+worldXSpeed);
 		g.get(3).setX(g.get(3).getX()+worldXSpeed);
+		g.get(4).setX(g.get(4).getX()+worldXSpeed);
+		g.get(5).setX(g.get(5).getX()+worldXSpeed);
 		
 		if((g.get(1).getX()) <= 0) {
 			g.get(1).setX(800);
@@ -49,7 +51,7 @@ public class SideScrollModel extends Model {
 	}
 	
 	public void advanceBird(ArrayList<GameObject> g, int yDirec) {
-			g.get(0).setY(g.get(0).getY()+osprey.getBirdSpeed()*yDirec);
+			g.get(0).setY(g.get(0).getY()+yDirec);
 	}
 	/*
 	public void dive(ArrayList<GameObject> g) {
@@ -72,7 +74,8 @@ public class SideScrollModel extends Model {
 	 * this method will contain the logic for advancing the icon on 
 	 * the minimap to show progress during the birds migration
 	 */
-	public void miniMap() {
+	public void miniMap(ArrayList<GameObject> g) {
+		g.get(0).setY(g.get(0).getY() + 5);
 	}
 	
 	public static void main(String[] args) {
