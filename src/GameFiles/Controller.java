85b56058d931a91a049323027740bf3039a51a36
afthompson <andrewft@udel.edu>
2019-05-02 02:15:00 -0400

    added some of SelectionView, going to bed

--- src/GameFiles/Controller.java
+++ src/GameFiles/Controller.java
@@ -186,6 +186,7 @@ public class Controller implements KeyListener{
 			topDownModel.setyChg(0);
 		}
 	}
+
 	
 	
 }
--- src/GameFiles/SelectionView.java
+++ src/GameFiles/SelectionView.java
@@ -1,9 +1,17 @@
 package GameFiles;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 /**
  * View for the selection screen
  */
 public class SelectionView extends View{
+	
 	/**
 	 * button that will allow user to select the osprey game
 	 */
@@ -12,16 +20,22 @@ public class SelectionView extends View{
 	 * button that will allow user to select the clapperrail game
 	 */
 	JButton clapperRail;
-	public JButton getOsprey() {
-		return osprey;
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	public SelectionView() {
+		
+		osprey = new JButton(new ImageIcon("selectOsprey.png"));
+		clapperRail = new JButton(new ImageIcon("selectCR.png"));
+		this.setLayout(new FlowLayout());
+		this.add(osprey);
+		this.add(clapperRail);
+		this.setSize(screenSize.getSize());
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
 	}
-	public void setOsprey(JButton osprey) {
-		this.osprey = osprey;
+	public static void main(String[] args) {
+		View sv = new SelectionView();
 	}
-	public JButton getClapperRail() {
-		return clapperRail;
-	}
-	public void setClapperRail(JButton clapperRail) {
-		this.clapperRail = clapperRail;
-	} 
 }
