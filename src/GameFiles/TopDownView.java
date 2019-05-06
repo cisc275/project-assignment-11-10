31b5470162fb6bd34421ac2dc09cdbf35021b49f
afthompson <andrewft@udel.edu>
2019-05-05 23:49:28 -0400

    modified bush image and made stick have green background

--- src/GameFiles/TopDownView.java
+++ src/GameFiles/TopDownView.java
@@ -60,7 +60,7 @@ public class TopDownView extends View{
 		
 			for (GameObject hing : game) {	
 				g.setClip(hing.hitbox);
-				g.drawImage(hing.getScaledImg(),hing.hitbox.xpoints[0] ,hing.hitbox.ypoints[0] , Color.gray, this);
+				g.drawImage(hing.getScaledImg(),hing.hitbox.xpoints[0] ,hing.hitbox.ypoints[0] , new Color(34, 177, 76), this);
 				//g.drawPolygon(hing.hitbox);
 			}
 	/*		
