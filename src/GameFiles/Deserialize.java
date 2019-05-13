299b591eb77f8f621c7185f404793dfc4e7b13f5
NCC74656 <wolginmark@gmail.com>
2019-05-13 18:09:04 -0400

    FIxed

--- src/GameFiles/Deserialize.java
+++ src/GameFiles/Deserialize.java
@@ -7,7 +7,7 @@ public class Deserialize
 {
     public static void grabGame() throws Exception
     {
-        FileInputStream fis = new FileInputStream("/dump/gameDump.txt");
+        FileInputStream fis = new FileInputStream("dump/gameDump.txt");
         ObjectInputStream ois = new ObjectInputStream(fis);
         ArrayList<GameObject> game = (ArrayList<GameObject>) ois.readObject();
         ois.close();
--- src/GameFiles/Serialize.java
+++ src/GameFiles/Serialize.java
@@ -8,7 +8,7 @@ public class Serialize
 {
     public static void dumpGame(ArrayList<GameObject> game) throws Exception
     {
-        FileOutputStream fos = new FileOutputStream("/dump/gameDump.txt");
+        FileOutputStream fos = new FileOutputStream("dump/gameDump.txt");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(game);
         oos.close();
