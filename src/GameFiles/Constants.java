c06fa843ddf09f14f6c7c2fdfcfe042d616c0049
NCC74656 <wolginmark@gmail.com>
2019-05-13 17:35:14 -0400

    BIgger for mark

--- src/GameFiles/Constants.java
+++ src/GameFiles/Constants.java
@@ -34,7 +34,7 @@ public class Constants implements Serializable {
 	public final static int FOX_START_SIZE_X = 100;
 	public final static int FOX_START_SIZE_Y = 100;
 	
-	public final static int LINUX_SCREEN_SIZE = 800;
+	public final static int LINUX_SCREEN_SIZE = 1600;
 	
 	public final static int OSPREY_DIVESPEED = 50;
 	public final static int OSPREY_MAX_HEIGHT = 600;
