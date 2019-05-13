7036ff6abf754d37f48334132d499e11d2810f24
jennyb1 <46670398+jennyb1@users.noreply.github.com>
2019-05-13 17:52:49 -0400

    forgot this for last push

--- src/GameFiles/EndScreen.java
+++ src/GameFiles/EndScreen.java
@@ -1,9 +1,36 @@
 package GameFiles;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class EndScreen implements ActionListener {
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class EndScreen extends JDialog implements ActionListener {
+	
+	JLabel congrats;
+	JButton menu;
+	JPanel p;
+	
+	
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	
+	
+	public EndScreen() {
+		p = new JPanel();
+		p.setOpaque(true);
+		p.setBackground(Color.WHITE);
+		p.setLayout(new BoxLayout(p, 3));
+		this.setResizable(false);
+		this.setModal(true);
+		
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
