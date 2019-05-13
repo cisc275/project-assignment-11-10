c1f405b92005f9d3e50771a6ce413f3adc8e0894
NCC74656 <wolginmark@gmail.com>
2019-05-13 17:39:48 -0400

    For the tutorial objects

---
+++ src/GameFiles/TutorialObject.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package GameFiles;
+
+/**
+ * @author wolginm
+ *
+ */
+public class TutorialObject extends NonControllable {
+
+	/**
+	 * @param x
+	 * @param y
+	 * @param width
+	 * @param height
+	 */
+	public TutorialObject(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+}
