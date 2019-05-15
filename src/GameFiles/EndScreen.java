b181456f87b9ebd787335feffe9ac170b25ffc77
jennyb1 <46670398+jennyb1@users.noreply.github.com>
2019-05-14 22:55:07 -0400

    brought endscreen into master

---
+++ src/GameFiles/EndScreen.java
@@ -0,0 +1,89 @@
+    
+package GameFiles;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.ImageIcon;
+import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class EndScreen extends JDialog implements ActionListener {
+	
+	
+	Image image;
+	JLabel congrats;
+	JButton menu;
+	JPanel p;
+	
+	
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	
+	
+	public EndScreen() {
+		p = new JPanel();
+		p.setOpaque(true);
+		p.setBackground(Color.WHITE);
+		p.setLayout(new BoxLayout(p, 3));
+		this.setResizable(false);
+		this.setModal(true);
+		congrats = new JLabel("Congradulations! You Win!");
+		congrats.setOpaque(false);
+        congrats.setFont(new Font("Serif", Font.BOLD, 30));
+        congrats.setMinimumSize(new Dimension(550, 50));
+        congrats.setPreferredSize(new Dimension(550, 50));
+        congrats.setMaximumSize(new Dimension(550, 50));
+        congrats.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        congrats.setAlignmentY(JLabel.TOP_ALIGNMENT);
+        
+        menu = new JButton("Return to Selection Screen");
+        menu.setMinimumSize(new Dimension(500, 50));
+        menu.setPreferredSize(new Dimension(500, 50));
+        menu.setMaximumSize(new Dimension(500, 50));
+    	menu.setFocusPainted(false);
+    	menu.setBackground(Color.WHITE);
+    	menu.setAlignmentX(JButton.CENTER_ALIGNMENT);
+    	menu.setAlignmentY(TOP_ALIGNMENT);
+    	menu.setOpaque(true);
+    	menu.setFont(new Font("Serif", Font.BOLD, 30));
+    	menu.setBorderPainted(false);
+        
+        p.add(congrats);
+        p.add(menu);
+        
+        menu.addActionListener(this);
+        this.add(p);
+        this.setSize(1920, 1080);
+        this.setVisible(true);
+        
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == menu) { 
+            // create a dialog Box 
+            System.out.println("send to menu");
+            this.dispose();
+            this.setModal(false);
+            Stick.count = 0;
+            Controller c = new Controller("sel");
+        }
+
+	}
+	
+
+}
