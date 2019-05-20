46661819fbbd5c42373c67574e7d263df11b6797
Timothy Mazzarelli <tmazzar@udel.edu>
2019-05-20 02:15:31 -0400

    controller change, hopefully skip over

--- src/GameFiles/LoseScreen.java
+++ src/GameFiles/LoseScreen.java
@@ -14,14 +14,21 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.swing.ImageIcon;
+import javax.swing.InputMap;
 import javax.imageio.ImageIO;
+import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
-public class LoseScreen extends JDialog implements ActionListener {
+import GameFiles.Quiz.keyAction;
+
+public class LoseScreen extends JDialog implements KeyListener {
 
 	Image image;
 	JLabel tryAgain;
@@ -82,8 +89,8 @@ public class LoseScreen extends JDialog implements ActionListener {
 			p.add(menu);
 			p.add(restart);
 
-			menu.addActionListener(this);
-			restart.addActionListener(this);
+			menu.addKeyListener(this);
+			restart.addKeyListener(this);
 			this.add(p);
 			this.setSize(screenSize);
 			this.setVisible(true);
@@ -132,8 +139,8 @@ public class LoseScreen extends JDialog implements ActionListener {
 			p.add(menu);
 			p.add(restart);
 
-			menu.addActionListener(this);
-			restart.addActionListener(this);
+			menu.addKeyListener(this);
+			restart.addKeyListener(this);
 			this.add(p);
 			this.setSize(screenSize);
 			this.setVisible(true);
@@ -146,64 +153,87 @@ public class LoseScreen extends JDialog implements ActionListener {
 	 * if menu clicked, returns to the selection screen, if restart is clicked, new
 	 * CR game is started
 	 */
+	private class keyAction extends AbstractAction{
 		  @Override 
 		  public void actionPerformed(ActionEvent e) { 
+			  endtheScreen();
 			  if(game.equals("cr")) {
 				  if (e.getSource() == menu) { 
-					  System.out.println("send to menu"); this.dispose();
-					  this.setModal(false); 
+					  System.out.println("send to menu"); 
 					  Stick.count = 0; 
 					  ClapperRail.lives = 3; 
 					  Controller c = new Controller("sel"); 
 				  } 
 				  else if (e.getSource() == restart) {
 					  System.out.println("restart"); 
-					  this.dispose(); 
-					  this.setModal(false);
+					  
 					  Stick.count = 0; 
 					  ClapperRail.lives = 3; 
-					  Controller c = new Controller("topDown");
+					  Controller c = new Controller("topDownNT");
 				  }
 			  }
 			  if(game.equals("os")) {
 				  if (e.getSource() == menu) { 
 					  System.out.println("send to menu"); 
-					  this.setModal(false); 
 					  Mate.caughtUp = false;
 					  Controller c = new Controller("sel"); 
-					  this.dispose();
+					  
 				  } 
 				  else if (e.getSource() == restart) {
-					  System.out.println("restart"); 
-					  this.setModal(false); 
+					  System.out.println("restart");  
 					  Mate.caughtUp = false;
 					  new Controller("sideScrollNT");
-					  this.dispose(); }
+					   }
 			  }
 			  
+			  
+			  
 		  }
+	}
+	
+	public void endtheScreen() {
+		this.dispose();
+		this.setModal(false);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		menuSetup();
+		restartSetup();
+	}
+	
+	public void menuSetup() {
+		int map = JComponent.WHEN_IN_FOCUSED_WINDOW;
+		InputMap imap = menu.getInputMap(map);
+		imap.put(KeyStroke.getKeyStroke("RIGHT"), "menu");
+		
+		ActionMap amap = menu.getActionMap();
+		amap.put("menu", new keyAction());
+	}
+	
+	public void restartSetup() {
+		int map = JComponent.WHEN_IN_FOCUSED_WINDOW;
+		InputMap imap = restart.getInputMap(map);
+		imap.put(KeyStroke.getKeyStroke("LEFT"), "tryAgain");
+		
+		ActionMap amap = restart.getActionMap();
+		amap.put("tryAgain", new keyAction());
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 		 
 
-	/*
-	 * @Override public void keyPressed(KeyEvent e) { System.out.println("KP");
-	 * switch (e.getKeyCode()) { //left case 37 :
-	 * System.out.println("send to menu"); this.dispose(); this.setModal(false);
-	 * Stick.count = 0; ClapperRail.lives = 3; Controller c = new Controller("sel");
-	 * //right case 39 : System.out.println("send to menu"); this.dispose();
-	 * this.setModal(false); Stick.count = 0; ClapperRail.lives = 3; Controller d =
-	 * new Controller("topDown"); }
-	 * 
-	 * }
-	 * 
-	 * @Override public void keyReleased(KeyEvent e) { // TODO Auto-generated method
-	 * stub
-	 * 
-	 * }
-	 * 
-	 * @Override public void keyTyped(KeyEvent e) { // TODO Auto-generated method
-	 * stub
-	 * 
-	 * }
-	 */
+	
+	 
 
 }
