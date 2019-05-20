cb8335f6e09d9949b8347f359821e6e06dedb918
jennyb1 <46670398+jennyb1@users.noreply.github.com>
2019-05-19 21:14:27 -0400

    deleted excess collision in CR

--- src/GameFiles/ClapperRail.java
+++ src/GameFiles/ClapperRail.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 /**
- * represents the clapperrail for the side scroll game 
+ * represents the clapperrail for the side scroll game
  *
  */
 public class ClapperRail extends Bird {
@@ -17,153 +17,155 @@ public class ClapperRail extends Bird {
 	/**
 	 * describes if the bird's coordinates matches with that of a bush on screen
 	 */
-	boolean hidden = false; 
+	boolean hidden = false;
 	/**
 	 * describes if the bird has a stick
 	 */
-	boolean carryStick; 
-	
+	boolean carryStick;
+
 	/**
 	 * the number of lives the clapper rail has
 	 */
 	static int lives = Constants.CLAPPER_RAIL_INIT_LIVES;
-	
+
 	/**
 	 * the bush that is closest to the clapper rail
 	 */
 	Bush closestBush;
-	
+
 	/**
 	 * an array of Bushes
 	 */
 	ArrayList<Bush> bushArr = new ArrayList<Bush>();
-	
-	
+
 	/**
 	 * @param y
 	 * @param x
 	 * @param width
 	 * @param height
 	 * 
-	 * a constructor that takes values for all fields as input parameters
+	 *               a constructor that takes values for all fields as input
+	 *               parameters
 	 */
-	
+
 	public ClapperRail(int x, int y, int width, int height) {
-		super(x,y,width, height);
+		super(x, y, width, height);
 		this.setType(Type.CLAPPERRAIL);
 		this.xSpeed = 0;
 		this.ySpeed = 0;
 		this.imgFileName = Constants.ANIMATION_CR;
 		this.curImg = randy.nextInt(imgFileName.length);
-		//this.img = createImage();
+		// this.img = createImage();
 	}
-	
-	
+
 	/**
 	 * moves clapperrail according to x and y speed
+	 * 
 	 * @param none
 	 * @author tim Mazzarelli
 	 */
-  
+
 	public void move() {
 		this.hitbox.translate(this.xSpeed, this.ySpeed);
 		boundaries();
-		if(!(this.xSpeed==0 && this.ySpeed==0)) {
+		if (!(this.xSpeed == 0 && this.ySpeed == 0)) {
 			this.animate(Constants.CLAPPER_RAIL_ANIMATION_TICK_RATE);
 		}
 	}
+
 	@Override
 	public void animate(int constant) {
-		int modifier = this.xSpeed>0? this.imgFileName.length/2 : 0;
-		this.curImgTickCount ++;
+		int modifier = this.xSpeed > 0 ? this.imgFileName.length / 2 : 0;
+		this.curImgTickCount++;
 		if (curImgTickCount == constant) {
-			curImg = modifier + ((curImg + 1) % (this.imgFileName.length/2));
-			//System.out.println(curImg);
+			curImg = modifier + ((curImg + 1) % (this.imgFileName.length / 2));
+			// System.out.println(curImg);
 			curImgTickCount = 0;
-			//this.visible = false;
-		}	
+			// this.visible = false;
+		}
 	}
-	
+
 	/**
-	 * handles collision with fox f 
+	 * handles collision with fox f
+	 * 
 	 * @param f
 	 * @author tim Mazzarelli
-	 */
-	
-	public void handleCollision(Fox f) {
-		resetPoly();
-	
-	}
-	
-	
+	 *//*
+		 * 
+		 * public void handleCollision(Fox f) { resetPoly();
+		 * 
+		 * }
+		 */
+
 	/**
-	 * @param takes an ArrayList of GameObject and checks each object (not itself) for collisions
+	 * @param takes an ArrayList of GameObject and checks each object (not itself)
+	 *              for collisions
 	 * @author andrew thompson
 	 */
 	@Override
 	public void collision(ArrayList<GameObject> g) {
 		for (GameObject a : g) {
-			if (this.collidesWith(a)){
+			if (this.collidesWith(a)) {
 				a.handleCollision(this);
 			}
 		}
 		findClosestBush();
 		stillHiding();
-		
+
 	}
-	
-	
+
 	/**
 	 * searches through the bushArr for the closest, sets result as closestBush
+	 * 
 	 * @author Peter Jenny
 	 */
 	public void findClosestBush() {
 		double d = 0;
 		Bush close = null;
-		for(Bush b : bushArr) {
+		for (Bush b : bushArr) {
 			double tmp;
 			// distance formula
 			double i = Math.pow(b.getX() - hitbox.xpoints[0], 2.0) + Math.pow((b.getY() - hitbox.ypoints[0]), 2.0);
 			tmp = Math.sqrt(i);
-			//System.out.print(b + ";           ");
-			if(tmp < d || d == 0) {
+			// System.out.print(b + "; ");
+			if (tmp < d || d == 0) {
 				d = tmp;
 				close = b;
 			}
 		}
-		/*System.out.println();
-		System.out.println("CR ========== " + hitbox.getBounds());
-		*/closestBush = close;/*
-		System.out.println(close);
-		System.out.println();*/
-		
+		/*
+		 * System.out.println(); System.out.println("CR ========== " +
+		 * hitbox.getBounds());
+		 */closestBush = close;/*
+								 * System.out.println(close); System.out.println();
+								 */
+
 	}
-	
-	
+
 	/**
 	 * checks to see if the CR is still hiding in the nearest bush
+	 * 
 	 * @author Peter Jenny
 	 */
-	public void stillHiding(){
-		if (!this.collidesWith(closestBush)){
-			
+	public void stillHiding() {
+		if (!this.collidesWith(closestBush)) {
+
 			hidden = false;
-		}
-		else {
-			
+		} else {
+
 		}
 	}
-	
+
 	/**
 	 * 
 	 * @param hidden
 	 */
-	
+
 	public void setHidden(boolean hidden) {
-		
+
 		this.hidden = hidden;
 	}
-	
+
 	/**
 	 * 
 	 * @return carryStick
@@ -180,15 +182,13 @@ public class ClapperRail extends Bird {
 	public void setCarryStick(boolean carryStick) {
 		this.carryStick = carryStick;
 	}
-		
-	
+
 	/**
 	 * @return the xSpeed
 	 */
 	public int getxSpeed() {
 		return xSpeed;
 	}
-
 
 	/**
 	 * @param xSpeed the xSpeed to set
@@ -197,7 +197,6 @@ public class ClapperRail extends Bird {
 		this.xSpeed = xSpeed;
 	}
 
-
 	/**
 	 * @return the ySpeed
 	 */
@@ -205,14 +204,13 @@ public class ClapperRail extends Bird {
 		return ySpeed;
 	}
 
-
 	/**
 	 * @param ySpeed the ySpeed to set
 	 */
 	public void setySpeed(int ySpeed) {
 		this.ySpeed = ySpeed;
 	}
-	
+
 //	private BufferedImage createImage(){
 //    	BufferedImage bufferedImage;
 //    	try {
