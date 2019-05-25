b442f133d085755b6538bdc280dc644d86b0c258
NCC74656 <wolginmark@gmail.com>
2019-05-25 18:26:38 -0400

    More Commits

--- src/GameFiles/TopDownModel.java
+++ src/GameFiles/TopDownModel.java
@@ -191,7 +191,7 @@ public class TopDownModel extends Model {
     protected void postTutorial() {
     	f = new Fox(Constants.FOX_START_LOC_X, Constants.FOX_START_LOC_Y, Constants.FOX_START_SIZE_X, Constants.FOX_START_SIZE_Y, cr);
     	game.add(f);
-    	game.add(new Powerup(Constants.FRAME_X - Constants.STICK_OFFSET_X[0], Constants.FRAME_Y - (int)(Constants.POWERUP_HEIGH * Constants.POWERUP_HEIGH_SCALE),
+    	game.add(new Powerup(Constants.FRAME_X - Constants.STICK_OFFSET_X[0], Constants.FRAME_Y - (int)(Constants.POWERUP_HEIGH * Constants.POWERUP_HEIGH_SCALE) - Constants.STICK_OFFSET_Y[0],
 					(int)(Constants.POWERUP_SIZE * Constants.POWERUP_SCALE), (int)(Constants.POWERUP_SIZE * Constants.POWERUP_SCALE)));
 		game.add(new Stick((int) ((Constants.FRAME_X * .5) - Constants.STICK_OFFSET_X[1]), Constants.STICK_OFFSET_Y[1],
 					(int)(Constants.STICK_SIZE * Constants.STICK_SCALE), (int)(Constants.STICK_SIZE * Constants.STICK_SCALE)));
